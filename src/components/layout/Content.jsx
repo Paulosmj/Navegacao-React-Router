6c499e15b45bd2c936ce92ced67d33ba0084dac4
Paulosmj <psmeneguesso@gmail.com>
2022-01-20 15:28:34 -0300

    Estrutura do projeto

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+import React from 'react';
+import './Content.css';
+
+const Content = (props) => <main className="Content">Componente Content</main>;
+
+export default Content;
---
+++ src/views/App.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+import React from 'react';
+import './App.css';
+
+const App = (props) => <div className="App">Componente App</div>;
+
+export default App;
