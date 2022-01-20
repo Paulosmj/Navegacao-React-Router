7ddc2b4b88014d3c29c37c21d1ac05d3737c9b95
Paulosmj <psmeneguesso@gmail.com>
2022-01-20 16:06:24 -0300

    Aplicando estilo

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -2,7 +2,14 @@
 
 import React from 'react';
 import './App.css';
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
 
-const App = (props) => <div className="App">Componente App</div>;
+const App = (props) => (
+  <div className="App">
+    <Menu />
+    <Content />
+  </div>
+);
 
 export default App;
